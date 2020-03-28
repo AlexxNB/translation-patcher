f41014643582a904a295c19c1d38d0c06c901553
AlexxNB <iam@alexxnb.ru>
2020-03-28 16:27:28 +0300

    make run asyncable

--- src/run.js
+++ src/run.js
@@ -3,19 +3,24 @@ const cp = require('child_process');
 export default async function (command,cwd) {    
     const commands = command.trim().split(' ');
     
-    const child = cp.spawn(commands.shift(), commands, {
-        stdio: ['ignore', 'inherit', 'inherit'],
-        shell: true,
-        detached: true,
-        cwd
+    return await new Promise((resolve,reject)=>{
+        const child = cp.spawn(commands.shift(), commands, {
+            stdio: ['ignore', 'inherit', 'inherit'],
+            shell: true,
+            detached: true,
+            cwd
+        });
+
+        child.on('uncaughtException',err=>reject(err));
+        child.on('exit',()=>resolve(true));
+    
+        process.on('uncaughtException',(err)=>{
+            process.kill(-child.pid) ;
+            throw err;
+        })
+    
+        process.on('exit',()=>{
+            process.kill(-child.pid) ;
+        })
     });
-
-    process.on('uncaughtException',(err)=>{
-        process.kill(-child.pid) ;
-        throw err;
-    })
-
-    process.on('exit',()=>{
-        process.kill(-child.pid) ;
-    })
 }
