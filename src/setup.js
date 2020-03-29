513effea05f31caa469ff3b8bc48f7a6a359a680
AlexxNB <iam@alexxnb.ru>
2020-03-29 13:09:33 +0300

    fix config setup param name

--- src/setup.js
+++ src/setup.js
@@ -3,6 +3,6 @@ import run from './run';
 
 export default async function (config) {
     log.stage(`Install dependencies`);
-    log.ok(`Running command: ${log.i(config.install)}`);
-    run(config.install,config.builddir);
+    log.ok(`Running command: ${log.i(config.setup)}`);
+    run(config.setup,config.builddir);
 }
