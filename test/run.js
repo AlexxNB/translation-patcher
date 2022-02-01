cbcb4e1d72832e83d6b58a23c8b6fb5d4e995758
AlexxNB <iam@alexxnb.ru>
2022-02-01 09:16:34 +0300

    Use uvu for tests

--- test/run.js
+++ test/run.js
@@ -1,85 +1,95 @@
 const cp = require('child_process');
-const test = require('tape');
+const { test } = require('uvu');
+const { ok } = require('uvu/assert');
+
 const fs = require('fs-extra');
 const path = require('path');
 const c = require('ansi-colors');
 
-const DIRNAME = 'test_site';
+const DIRNAME = '_test_site';
 const TRPATCHER = 'node '+path.resolve(path.join('bin','patcher.js'));
 
 const TESTDIR = path.join('test',DIRNAME);
 const PATCHDIR = path.join('test','patch');
 const BUILDDIR = path.join(TESTDIR,'__BUILD');
 
-(async()=>{
-   
+ 
+test('trpatcher init',async ()=>{
+    await run(`${TRPATCHER} init ${DIRNAME} -n`,'test');
+    ok(
+        fs.existsSync(TESTDIR),
+        'Directory created'
+    );
+    ok(
+        fs.existsSync(path.join(TESTDIR,'translation.config.json')),
+        'Template downloaded'
+    );
+
+    fs.copySync(PATCHDIR,path.join(TESTDIR,'patch'));
+    ok(
+        fs.existsSync(path.join(TESTDIR,'patch','src','Unexistent.svelte')),
+        'Testing patch directory copied'
+    );
+});
+
+test('trpatcher download',async ()=>{
+    await run(`${TRPATCHER} download`,TESTDIR);
+    ok(
+        fs.existsSync(path.join(TESTDIR,'__BUILD','package.json')),
+        'Example site repository downloaded'
+    );
+});
+
+
+test('trpatcher apply', async ()=>{
     
-    test('Testing trpatcher', async (t)=>{
-        
-        await run(`${TRPATCHER} init ${DIRNAME} -n`,'test');
-        t.ok(
-            fs.existsSync(TESTDIR),
-            'Directory created'
-        );
-        t.ok(
-            fs.existsSync(path.join(TESTDIR,'translation.config.json')),
-            'Template downloaded'
-        );
+    const apply_result = await run(`${TRPATCHER} apply`,TESTDIR);
+    ok(
+        apply_result.includes(`Replaced src/App.svelte`),
+        'File replaced info'
+    )
+    ok(
+        apply_result.includes(`src/Unexistent.svelte doesn't exists in original site`),
+        'Unexistent file warn'
+    )
+    ok(
+        apply_result.includes(`String "name: 'world'" replaced by "name: 'Мир'"`),
+        'String in a file replaced info'
+    )
+    ok(
+        apply_result.includes(`String "unexistent string" not found`),
+        'Unexistent sring warn'
+    )
+    ok(
+        apply_result.includes(`File src/this_file_is_not_exists.svelte doesn't exists`),
+        'Unexistent file for strings warn'
+    )
+    ok(
+        apply_result.includes(`No strings provided`),
+        'No strings warn'
+    )
 
-        fs.copySync(PATCHDIR,path.join(TESTDIR,'patch'));
-        t.ok(
-            fs.existsSync(path.join(TESTDIR,'patch','src','Unexistent.svelte')),
-            'Testing patch directory copied'
-        );
+    const string_file = fs.readFileSync(path.join(BUILDDIR,'src','main.js'),'utf-8');
+    ok(
+        string_file.includes(`name: 'Мир'`),
+        'String in file replaced'
+    )
 
-        await run(`${TRPATCHER} download`,TESTDIR);
-        t.ok(
-            fs.existsSync(path.join(TESTDIR,'__BUILD','package.json')),
-            'Example site repository downloaded'
-        );
+    const file = fs.readFileSync(path.join(BUILDDIR,'src','App.svelte'),'utf-8');
+    ok(
+        file.includes(`Привет {name}!`),
+        'Whole file replaced'
+    )
 
-        const apply_result = await run(`${TRPATCHER} apply`,TESTDIR);
-        t.ok(
-            apply_result.includes(`Replaced src/App.svelte`),
-            'File replaced info'
-        )
-        t.ok(
-            apply_result.includes(`src/Unexistent.svelte doesn't exists in original site`),
-            'Unexistent file warn'
-        )
-        t.ok(
-            apply_result.includes(`String "name: 'world'" replaced by "name: 'Мир'"`),
-            'String in a file replaced info'
-        )
-        t.ok(
-            apply_result.includes(`String "unexistent string" not found`),
-            'Unexistent sring warn'
-        )
-        t.ok(
-            apply_result.includes(`File src/this_file_is_not_exists.svelte doesn't exists`),
-            'Unexistent file for strings warn'
-        )
-        t.ok(
-            apply_result.includes(`No strings provided`),
-            'No strings warn'
-        )
+    
+});
 
-        const string_file = fs.readFileSync(path.join(BUILDDIR,'src','main.js'),'utf-8');
-        t.ok(
-            string_file.includes(`name: 'Мир'`),
-            'String in file replaced'
-        )
+test.after(()=>{
+    fs.removeSync(TESTDIR);
+});
 
-        const file = fs.readFileSync(path.join(BUILDDIR,'src','App.svelte'),'utf-8');
-        t.ok(
-            file.includes(`Привет {name}!`),
-            'Whole file replaced'
-        )
+test.run();
 
-        fs.removeSync(TESTDIR);
-        t.end();
-    });
-})();
 
 
 async function run(command,cwd='.') {    
